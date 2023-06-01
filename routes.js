7a39ff71c827d5f3941e46dcfc0bd3df50fa6d74
Thomas Lautaro López <laautarolopez@gmail.com>
2023-06-01 19:24:36 -0300

    johny five agregado

--- routes.js
+++ routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { SerialPort, ReadlineParser } from 'serialport';
+import five from "johnny-five";
 const path = '/dev/ttyACM0'
 const baudRate = 9600
 const port = new SerialPort({ path, baudRate })
 
 const router = Router();
 
+const board = new five.Board();
+
 router.post('/prender', (req, res) => {
-    port.write('B');
+    board.on("ready", () => {
+        const blueLed = new Led(7);
+        const redLed = new Led(8);
+        // Blink every half second
+        blueLed.on();
+        redLed.on();
+    });
     res.send("OK");
 })
 
